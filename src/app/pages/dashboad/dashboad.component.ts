ec32f7ca0fd256228ddbd9bbaec8fafa231330a3
luissangge <lacarbonel2017@gmail.com>
2020-04-26 17:56:19 -0300

    Firts Commit

---
+++ src/app/pages/dashboad/dashboad.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboad',
+  templateUrl: './dashboad.component.html',
+  styles: [
+  ]
+})
+export class DashboadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
